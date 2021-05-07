768284470298ff6a45f7235c45b3c971f207f7e6
Kasper <mirzohid.rahmatullaev@gmail.com>
2021-05-07 16:43:51 +0500

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,34 +1,54 @@
 const hotelSlider = new Swiper('.hotel-slider', {
-	// Optional parameters
-	loop: true,
+  // Optional parameters
+  loop: true,
 
-	// Navigation arrows
-	navigation: {
-		prevEl: '.hotel-slider__button-prev',
-		nextEl: '.hotel-slider__button-next',
-	},
-	keyboard: {
+  // Navigation arrows
+  navigation: {
+    prevEl: '.hotel-slider__button-prev',
+    nextEl: '.hotel-slider__button-next',
+  },
+  keyboard: {
     enabled: true,
     onlyInViewport: false,
-		pageUpDown: true,
+    pageUpDown: true,
   },
-	effect: "coverflow",
-	speed: 550
-});
+  effect: 'coverflow',
+  speed: 550,
+})
 
 const reviewsSlider = new Swiper('.reviews-slider', {
-	// Optional parameters
-	loop: true,
+  // Optional parameters
+  loop: true,
 
-	// Navigation arrows
-	navigation: {
-		prevEl: '.reviews-slider__button-prev',
-		nextEl: '.reviews-slider__button-next',
-	},
-	keyboard: {
+  // Navigation arrows
+  navigation: {
+    prevEl: '.reviews-slider__button-prev',
+    nextEl: '.reviews-slider__button-next',
+  },
+  keyboard: {
     enabled: true,
     onlyInViewport: false,
-		pageUpDown: true,
+    pageUpDown: true,
   },
-	speed: 550
-});
+  speed: 550,
+})
+
+var menuButton = document.querySelector('.menu-button')
+menuButton.addEventListener('click', function () {
+  console.log('Клик по кнопке')
+  document
+    .querySelector('.navbar-bottom')
+    .classList.toggle('navbar-bottom--visible');
+  document
+    .querySelector('body')
+    .classList.toggle('lock');
+  document
+    .querySelector('.menu-button--top--line')
+    .classList.toggle('menu-button--top');
+  document
+    .querySelector('.menu-button--bottom--line')
+    .classList.toggle('menu-button--bottom')
+  document
+    .querySelector('.menu-button--middle--line')
+    .classList.toggle('menu-button--middle')
+})
